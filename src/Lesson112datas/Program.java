091286e485d48de10542d2908ebf23fd72a099b2
Wagner Araújo <wagneraraujo15@gmail.com>
2023-10-01 12:02:43 -0300

    date

--- src/Lesson112datas/Program.java
+++ src/Lesson112datas/Program.java
@@ -1,2 +1,13 @@
-package Lesson112datas;public class Program {
+package Lesson112datas;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Program {
+    public static void main(String[] args){
+        LocalDate d04 = LocalDate.parse("2023-09-13");
+        LocalDateTime d05 = LocalDateTime.parse("2023-09-13T02:30:23");
+        Instant d06 = Instant.parse("2023-09-13T02:30:23Z");
+    }
 }
